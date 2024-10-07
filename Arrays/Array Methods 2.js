459def4e93f3448d2018c5c06b75da9a2493ba5d
ShaudonTHG <shaudonthgordon@gmail.com>
2024-10-07 14:49:29 +0100

    Add files via upload

---
+++ Arrays/Array Methods 2.js
@@ -0,0 +1,27 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript']
+
+// remove the last string
+//console.log(secretMessage.pop())
+
+// count the amount of string
+//console.log(secretMessage.length)
+
+// adding string at the end
+//secretMessage.push('to','Program ')
+
+// replace with the = string
+//secretMessage[7] = 'right'
+
+// remove the frist string
+//secretMessage.shift()
+
+// adding string at the start
+//secretMessage.unshift('Programming')
+
+// remove strings and replace with the = "string"
+//secretMessage.splice(6,5, "know,")
+
+// remove the comma from each strings a
+//console.log(secretMessage.join(' '))
+
+console.log(secretMessage)
