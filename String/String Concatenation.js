bfde925056ad425f29179a1af44ee3e1af4b36b8
ShaudonTHG <shaudonthgordon@gmail.com>
2024-10-07 01:42:51 +0100

    Add files via upload

---
+++ String/String Concatenation.js
@@ -0,0 +1,13 @@
+
+//example One
+
+console.log('Shau' + 'don')
+
+
+//example Two
+console.log('Shaudon ' + 'Gordon'); 
+
+
+//example Three
+console.log('Shaudon' + ' ' + 'Hyllam' + '-' + 'Gordon'); 
+
