54826a4764f64899fc73cd445981c0390d3d08a8
ShaudonTHG <shaudonthgordon@gmail.com>
2024-10-07 17:52:02 +0100

    Add files via upload

---
+++ Arrays/pop.js
@@ -0,0 +1,8 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+const removed = chores.pop();
+
+console.log(chores); 
+// Output: [ 'item 0', 'item 1' ]
+console.log(removed);
+// Output: item 2
